3acc4f7a15526ef3cb89aaecdbdf453396f1e384
Bohdan Zadorozhnyi <bohzador@gmail.com>
2021-12-28 19:36:19 +0200

    Implement graph visualization.

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### React-Ace
+We use [React-Ace](https://github.com/securingsincity/react-ace/tree/master) to automatically numerate rows of user input.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import stylesheet from './stylesheet.json';
+import React, { useState, useRef } from 'react'
+import CytoscapeComponent from 'react-cytoscapejs';
+import AceEditor from "react-ace";
+
+function updateGraph(userInput) {
+  let elements = [];
+  userInput.split('\n').forEach(function(row) {
+    let items = row.split(' ').filter(item => item.length > 0);
+    switch(items.length) {
+      case 0:
+        break;
+      case 1:
+        elements.push({ data: { id: items[0], label: items[0] } });
+        break;
+      case 2:
+        elements.push({ data: { id: items[0], label: items[0] } });
+        elements.push({ data: { id: items[1], label: items[1] } });
+        elements.push({ data: { source: items[0], target: items[1] } });
+        break;
+      case 3:
+        elements.push({ data: { id: items[0], label: items[0] } });
+        elements.push({ data: { id: items[1], label: items[1] } });
+        elements.push({ data: { source: items[0], target: items[1], label: items[2] } });
+        break;
+    }
+  });
+  return elements;
+}
 
 function App() {
+  const cyRef = useRef();
+  const [elements, setElements] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="graph-wrapper">
+        <CytoscapeComponent
+          elements={elements}
+          style={{
+            backgroundColor: 'green',
+            height: '100%',
+            width: '600px'
+          }}
+          stylesheet={stylesheet}
+          cy={(cy) => (cyRef.current = cy)}
+        />
+        <AceEditor
+          mode="javascript"
+          theme="github"
+          onChange={userInput => setElements(updateGraph(userInput))}
+        />
+      </div>
     </div>
   );
 }
